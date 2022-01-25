3f95541478cc7daaf93c0de28077dbf9c52e8b1c
Dylan Emerson <emersonds2868@gmail.com>
2022-01-25 01:30:59 -0700

    Added comments
     - Added comments to some spots that needed it.

--- EmersonIntroToPygame.py
+++ EmersonIntroToPygame.py
@@ -13,13 +13,15 @@ clock = pygame.time.Clock()
 background_color = (100, 149, 237)
 
 # Base square
-square_size = 60
+square_width = 60
+square_height = 60
 x = 30
 y = 30
 square_color = (27, 38, 79)
 
 # Gameplay loop
 while True:
+    # Quit the game when pygame is closed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -27,14 +29,14 @@ while True:
 
     # Move the square
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_w]: y -= 1   # positive y is down
-    if pressed[pygame.K_s]: y += 1
-    if pressed[pygame.K_a]: x -= 1
-    if pressed[pygame.K_d]: x += 1
+    if pressed[pygame.K_w]: y -= 1      # Up (positive y is down)
+    if pressed[pygame.K_s]: y += 1      # Down
+    if pressed[pygame.K_a]: x -= 1      # Left
+    if pressed[pygame.K_d]: x += 1      # Right
 
     # Fill screen and draw square
     screen.fill(background_color)
-    pygame.draw.rect(screen, square_color, pygame.Rect(x, y, square_size, square_size))
+    pygame.draw.rect(screen, square_color, pygame.Rect(x, y, square_width, square_height))
 
     # Refresh screen
     pygame.display.flip()
