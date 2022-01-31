fdafdc802820748ada8c4374fc0a15dee2f154ed
Dylan Emerson <emersonds2868@gmail.com>
2022-01-30 21:21:24 -0700

    Added Challenge 4 Looping Lists.py
     - Started working on Challenge 4: Looping Lists, which involves printing each element of a list forwards and backwards, as well as list comprehension.

---
+++ Challenge 4 Looping Lists.py
@@ -0,0 +1,10 @@
+numbers = [ "one", 2, 3.5, "four", 5 ]  # Initialize list
+print(numbers)                          # Print "numbers"
+
+# Print each item in numbers individually on a new line
+for number in numbers:
+    print(number)
+
+# Print each item in numbers backwards using slice notation
+for number in numbers[::-1]:
+    print(number)
