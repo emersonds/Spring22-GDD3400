43e8b9835a1ef31334f2c2404e11176e4806ac10
Dylan Emerson <emersonds2868@gmail.com>
2022-01-30 21:34:23 -0700

    Challenge 4: List Comprehension
     - Worked on Question 2, based on list comprehension, and added comments.
     - Basic list comprehension: values = [value for value in range(1,11)]. This initializes a list with values from 1 to 10.
     - Can also use math: values = [value**2 for value in range(1,11)]. This initializes a list of squared values from 1 to 10 (1, 4, 9, etc..)

--- Challenge 4 Looping Lists.py
+++ Challenge 4 Looping Lists.py
@@ -1,3 +1,5 @@
+# Question 1:
+
 numbers = [ "one", 2, 3.5, "four", 5 ]  # Initialize list
 print(numbers)                          # Print "numbers"
 
@@ -8,3 +10,10 @@ for number in numbers:
 # Print each item in numbers backwards using slice notation
 for number in numbers[::-1]:
     print(number)
+
+# Question 2:
+
+cubes = [ number**3 for number in range(1, 11) ]    # List Comphrension. Fills a list of ten cubes starting at 1 to 10. 1**3, 2**3, etc.
+print(cubes)                                        # Print "cubes"
+numbers = [ number for number in range(1, 76) ]     # List Comprehension. List contains values from 1 to 75.
+print (sum(numbers))                                # Print sum of "numbers" from 1 to 75
